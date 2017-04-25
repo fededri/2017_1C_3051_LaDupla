505a3b0d313ea9d86936b5eaaf5a5693bcf07a8b
fedetorres <federico.torres@infinixsoft.com>
2017-04-25 12:45:57 -0300

    textos de status

--- TGC.Group/Hud/Hud.cs
+++ TGC.Group/Hud/Hud.cs
@@ -15,8 +15,10 @@ namespace TGC.Group.Hud
         TgcText2D vida;
         TgcText2D agua;
         TgcText2D energia;
+        TgcText2D hambre;
         CustomSprite hudSprite;
         Drawer2D drawer2D;
+        
 
         public Hud(String HudDir)
         {
@@ -24,26 +26,45 @@ namespace TGC.Group.Hud
             agua = new TgcText2D();
             energia = new TgcText2D();
             drawer2D = new Drawer2D();
-
-            Size tamanioTexto = new Size(40, 60);
-            vida.Size = tamanioTexto;
-            vida.changeFont(new System.Drawing.Font("ComicSands", 16, FontStyle.Bold));
-
-            vida.Color = Color.Red;
-            agua.Size = tamanioTexto;
-            agua.changeFont(new System.Drawing.Font("ComicSands", 16, FontStyle.Bold));
-            agua.Color = Color.Cyan;
-            energia.Size = tamanioTexto;
-            energia.changeFont(new System.Drawing.Font("ComicSands", 16, FontStyle.Bold));
-            energia.Color = Color.Yellow;
+            hambre = new TgcText2D();
+            
 
             //crear Sprite de hud
             hudSprite = new CustomSprite();
             hudSprite.Bitmap = new CustomBitmap(HudDir + "barras.png", D3DDevice.Instance.Device);
-            var textureSize = new Size(30,30);
+            var textureSize = new Size(30, 30);
             hudSprite.Scaling = new Vector2(0.5f, 0.5f);
-            
             hudSprite.Position = new Vector2(10, 20);
+
+            //textos
+            Size tamanioTexto = new Size(200, 10);
+            vida.Size = tamanioTexto;
+            vida.Align = TgcText2D.TextAlign.RIGHT;           
+            vida.changeFont(new System.Drawing.Font("ComicSands", 12, FontStyle.Bold));
+            vida.Position = new Point(60, 20);
+            vida.Color = Color.White;
+            vida.Text = "Vida: 100";
+
+            energia.Size = tamanioTexto;
+            energia.Align = TgcText2D.TextAlign.RIGHT;
+            energia.changeFont(new System.Drawing.Font("ComicSands", 12, FontStyle.Bold));
+            energia.Position = new Point(60, 50);
+            energia.Text = "Energia: 100";
+            energia.Color = Color.White;
+
+            agua.Size = tamanioTexto;
+            agua.Align = TgcText2D.TextAlign.RIGHT;
+            agua.Color = Color.White;
+            agua.Text = "Agua: 100";
+            agua.Position = new Point(60, 90);
+            agua.changeFont(new System.Drawing.Font("ComicSands", 12, FontStyle.Bold));
+
+            hambre.Size = tamanioTexto;
+            hambre.Align = TgcText2D.TextAlign.RIGHT;
+            hambre.Color = Color.White;
+            hambre.Text = "Hambre: 100";
+            hambre.Position = new Point(60, 125);
+            hambre.changeFont(new System.Drawing.Font("ComicSands", 12, FontStyle.Bold));
 
 
         }
@@ -53,7 +74,10 @@ namespace TGC.Group.Hud
         {
             drawer2D.BeginDrawSprite();
             drawer2D.DrawSprite(hudSprite);
-
+            vida.render();
+            energia.render();
+            agua.render();
+            hambre.render();
             drawer2D.EndDrawSprite();
         }
 
